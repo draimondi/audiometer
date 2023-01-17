cace8637560164a86813e7f49913e2cd94141696
Domenico Raimondi <draimondi@gmail.com>
2023-01-17 22:55:25 +0100

    Reduce complexity of main function

--- audiometer/__main__.py
+++ audiometer/__main__.py
@@ -8,6 +8,52 @@ from audiometer import audiometer
 PROGRAM_TITLE = "Hearing Exam"
 
 
+def program_launch():
+    """Create the initial window layout"""
+    default_layout = [
+        [sg.Button("Start Hearing Exam", key="start")],
+        [sg.Checkbox("Randomize the Exam", default=False, key="randomize")],
+    ]
+    window = sg.Window(PROGRAM_TITLE, default_layout, grab_anywhere=True, finalize=True)
+    window.Refresh()
+    return window
+
+
+def exam_start(randomize):
+    """Starts the actual hearing exam"""
+    test_layout = [
+        [sg.Text("Which ear do you hear sound from?")],
+        [
+            [
+                sg.Button("Left Ear", key="left"),
+                sg.Button("Right Ear", key="right"),
+            ]
+        ],
+        [sg.Text(size=(40, 1), key="output")],
+        [sg.Button("Stop Exam", key="stop")],
+    ]
+    window = sg.Window(PROGRAM_TITLE, test_layout, grab_anywhere=True, finalize=True)
+    window.Refresh()
+    exam = audiometer.ExamThread()
+    exam.daemon = True
+    exam.generate_exam_combinations(randomize)
+    exam.start()
+    return window, exam
+
+
+def detect_sound(window, exam, channel):
+    """Detects if sound is heard in the correct channel"""
+    if exam.sound_is_heard(channel):
+        window["output"].update(
+            f"Sound correctly detected from the {channel}", text_color="green"
+        )
+    else:
+        window["output"].update(
+            f"Incorrect. Sound is not coming from the {channel}",
+            text_color="red",
+        )
+
+
 def main():
     """Creates the main user interface"""
     sg.theme("dark grey 8")
@@ -20,56 +66,23 @@ def main():
         else:
             event = "program_launch"
 
-        if event in {"program_launch", "stop"}:
-            # Stop running exam if it exists
-            if window and exam:
-                exam.stop()
+        match event:
+            case "program_launch" | "stop":
+                # Stop running exam if it exists
+                if window and exam:
+                    exam.stop()
+                    window.close()
+                window = program_launch()
+
+            case "start":
                 window.close()
-            # Create the initial window layout
-            default_layout = [
-                [sg.Button("Start Hearing Exam", key="start")],
-                [sg.Checkbox("Randomize the Exam", default=False, key="randomize")],
-            ]
-            window = sg.Window(
-                PROGRAM_TITLE, default_layout, grab_anywhere=True, finalize=True
-            )
-            window.Refresh()
+                window, exam = exam_start(randomize=(values["randomize"]))
 
-        if event == "start":
-            window.close()
-            test_layout = [
-                [sg.Text("Which ear do you hear sound from?")],
-                [
-                    [
-                        sg.Button("Left Ear", key="left"),
-                        sg.Button("Right Ear", key="right"),
-                    ]
-                ],
-                [sg.Text(size=(40, 1), key="output")],
-                [sg.Button("Stop Exam", key="stop")],
-            ]
-            window = sg.Window(
-                PROGRAM_TITLE, test_layout, grab_anywhere=True, finalize=True
-            )
-            window.Refresh()
-            exam = audiometer.ExamThread()
-            exam.daemon = True
-            exam.generate_exam_combinations(randomize=(values["randomize"]))
-            exam.start()
+            case "right" | "left":
+                detect_sound(window, exam, event)
 
-        if event in {"right", "left"}:
-            if exam.sound_is_heard(event):
-                window["output"].update(
-                    f"Sound correctly detected from the {event}", text_color="green"
-                )
-            else:
-                window["output"].update(
-                    f"Incorrect. Sound is not coming from the {event}",
-                    text_color="red",
-                )
-
-        if event == sg.WINDOW_CLOSED:
-            break
+            case sg.WINDOW_CLOSED:
+                break
 
 
 if __name__ == "__main__":
